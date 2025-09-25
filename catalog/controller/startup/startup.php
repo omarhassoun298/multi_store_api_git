6cfc0d00f2fec2d9996e5b2086c4c466ebc446fc
omarhassoun <omarhassoun.ishtari@gmail.com>
2025-09-25 14:02:49 +0300

    .

--- catalog/controller/startup/startup.php
+++ catalog/controller/startup/startup.php
@@ -14,11 +14,12 @@ class ControllerStartupStartup extends Controller {
 
 	public function index() {
 		// Store
-		if ($this->request->server['HTTPS']) {
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "store WHERE REPLACE(`ssl`, 'www.', '') = '" . $this->db->escape('https://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/') . "'");
-		} else {
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "store WHERE REPLACE(`url`, 'www.', '') = '" . $this->db->escape('http://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/') . "'");
-		}
+		// if ($this->request->server['HTTPS']) {
+		// 	$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "store WHERE REPLACE(`ssl`, 'www.', '') = '" . $this->db->escape('https://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/') . "'");
+		// } else {
+		// 	$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "store WHERE REPLACE(`url`, 'www.', '') = '" . $this->db->escape('http://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/') . "'");
+		// }
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "store ");
 		
 		if (isset($this->request->get['store_id'])) {
 			$this->config->set('config_store_id', (int)$this->request->get['store_id']);
--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -692,7 +692,7 @@ public function getStores($data = array()) {
 $store_data = $this->cache->get('store');
 
 if (!$store_data) {
-	$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "store ORDER BY url");
+	$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "store ");
 
 	$store_data = $query->rows;
 
